fcfaae2ae70a4fe84d484b9d5c7d292873737ed9
koll00 <senjia1988@126.com>
2018-01-18 15:28:17 +0800

    add system check and west menu

---
+++ routes/toCheckSystem.js
@@ -0,0 +1,81 @@
+var exec = require('child_process').exec,
+    spawn = require('child_process').spawn,
+    url = require('url'),
+    querystring = require('querystring'),
+    fs= require('fs'),
+    iconv = require('iconv-lite')
+    ;
+
+var CHECKSYSTEM = 'CHECK SYSTEM';
+var AUTOCHECK = 'AUTO CHECK';
+var SUCCESS = 'SUCCESS';
+var DOWNLOAD = 'DOWNLOAD';
+
+exports.checkSystemInput = function(req, res, next){
+    console.log(CHECKSYSTEM);
+    var query = url.parse(req.url).query;
+    var param = querystring.parse(query);
+    if(query == null){
+        next();
+    }
+    if(param.session == null){
+        next();
+    }
+    
+    res.render('checkSystem', { title: CHECKSYSTEM});
+};
+
+exports.autoCheck = function(req, res, next){
+	console.log(AUTOCHECK);
+	
+	var query = url.parse(req.url).query;
+	var param = querystring.parse(query);
+	
+	var cmdStr='/tmp/nodejs/scripts/fixPart.sh';
+	var cmdStr='help';
+	
+	var systems=['CES','QPS'];
+	var itemsBuffer = '';
+	//var child = spawn(cmdStr, ['PL' + param.envID, systemID[param.systemID]]);
+	var child = spawn(cmdStr);
+	child.stdout.on('data', function(data){
+		itemsBuffer += data.toString();
+	});
+	
+	child.stderr.on('data', function(data){
+		res.render('findLog', { title: AUTOCHECK});
+	});
+		
+	child.on('close', function(data) {
+			res.render('checkSystem', { title: AUTOCHECK});
+	});
+};
+
+exports.autoCheck = function(req, res, next){
+    console.log(AUTOCHECK);
+    
+    var query = url.parse(req.url).query;
+    var param = querystring.parse(query);
+    
+    var cmdStr='/tmp/nodejs/scripts/fixPart.sh';
+    var cmdStr='help';
+    
+    var systems=['CES','QPS'];
+    var itemsBuffer = '';
+    //var child = spawn(cmdStr, ['PL' + param.envID, systemID[param.systemID]]);
+    var child = spawn(cmdStr);
+    child.stdout.on('data', function(data){
+        itemsBuffer += data.toString();
+    });
+    
+    child.stderr.on('data', function(data){
+        res.render('findLog', { title: AUTOCHECK});
+    });
+        
+    child.on('close', function(data) {
+            res.render('checkSystem', { title: AUTOCHECK});
+    });
+};
+exports.checkSuccess = function(req, res, next){
+    res.render('success', { title: SUCCESS});
+};
---
+++ routes/toLogs.js
@@ -0,0 +1,67 @@
+var exec = require('child_process').exec,
+    spawn = require('child_process').spawn,
+    url = require('url'),
+    querystring = require('querystring'),
+    fs= require('fs'),
+    iconv = require('iconv-lite')
+    ;
+
+var FINGLOG = 'FIND LOG';
+var SHOWLOG = 'SHOW LOG';
+var KIDDING = 'KIDDING';
+var DOWNLOAD = 'DOWNLOAD';
+
+exports.showLog = function(req, res){
+	console.log(SHOWLOG);
+	
+	var showLogQuery = url.parse(req.url).query;
+	var param = querystring.parse(showLogQuery);
+	
+	var cmdStr='/tmp/nodejs/scripts/cpftm.sh';
+	var cmdStr='help';
+	
+	var itemsBuffer = '';
+	var child = spawn(cmdStr);
+	child.stdout.on('data', function(data){
+		itemsBuffer += data.toString();
+	});
+	
+	child.stderr.on('data', function(data){
+		res.render('findLog', { title: FINGLOG, params: param });
+	});
+		
+	child.on('close', function(data) {
+		var itemString = itemsBuffer;
+		if(itemString.length == 0){
+			res.render('findLog', { title: FINGLOG, params: param });
+		}else{
+			var file ='.\\routes\\test.json';
+			var items = JSON.parse(iconv.decode(fs.readFileSync(file),'GBK'));
+			//var items = eval('('+itemString+')');
+			res.render('showLog', { title: SHOWLOG,  logs: items, params: param });
+		}
+	});
+};
+
+exports.findLog = function(req, res){
+	console.log(FINGLOG);
+	
+	var findLogQuery = url.parse(req.url).query;
+	var param = querystring.parse(findLogQuery);
+	
+	res.render('findLog', { title: FINGLOG, params: param  })
+};
+
+exports.downloadFiles = function(req, res,next){
+	console.log(DOWNLOAD);
+	
+	var urlQuery = url.parse(req.url).query;
+	var param = querystring.parse(urlQuery);
+	console.log(param.file);
+//	res.sendfile('./1.pdf','1.pdf');
+	res.download(__dirname+'../1.pdf','1.pdf');
+	//res.download('..//public\\1.pdf');
+//	res.download('..//public\\downloadFile\\1.pdf');
+	//res.sendfile('中国银联离线仿真使用指引.pdf', { root:__dirname+'../donwloadFile' });
+	//res.render('downloadFiles', { title: DOWNLOAD})
+};
